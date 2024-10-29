09f593624f68b592124db8223a13386f48ce395f
matheusvirissimo <matheus.virissimo@unesp.br>
2024-10-29 16:00:24 -0300

    feat: inicialização e destruição do grafo

--- grafosLista.c
+++ grafosLista.c
@@ -9,8 +9,37 @@ typedef struct no{
 typedef NO *ptr_no;
 
 typedef struct grafo{
-    ptr_no *adjacente;
+    ptr_no *adjacencia;
     int n;
 } GRAFO;
 
 typedef GRAFO *ptr_grafo;
+
+ptr_grafo criarGrafo(int n){
+    ptr_grafo grafo = malloc(sizeof(GRAFO));
+
+    grafo->n = n;
+    grafo->adjacencia = (n * sizeof(ptr_no));
+
+    for(int i = 0; i < n; i++){
+        grafo->adjacencia[i] = NULL;
+    }
+
+    return grafo;
+}
+
+void liberaLista(ptr_no lista){
+    if(lista != NULL){
+        liberaLista(lista->prox);
+        free(lista);
+    }
+}
+
+void destroiGrafo(ptr_grafo grafo){
+    for(int i = 0; i < grafo->n; i++){
+        liberaLista(grafo->adjacencia[i]);
+    }
+
+    free(grafo->adjacencia);
+    free(grafo);
+}
